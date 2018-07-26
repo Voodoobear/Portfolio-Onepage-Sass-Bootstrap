9b9c4ec01eb9442235145ef374fc4c0d7a45c2f2
Voodoobear <s.chalain@gmail.com>
2018-07-26 10:05:10 +0200

    Onepage terminé avec scss et js, reste à ajouter des infos réelles

---
+++ js/sebastiencamberou.js
@@ -0,0 +1,38 @@
+$(function() {
+	var words = [
+		'un développeur',
+		'un geek',
+		'un codeur',
+		'un caféholic annonyme',
+		'un créatif inspiré'
+	],
+	i = 0;
+
+	// For screens less or equals to medium (992px)
+	$(".navbar-responsive").toggle();
+
+	$(".menu").click(function() {
+		$(".navbar-responsive").toggle("slow");
+	});
+
+	// handdeling the Catch Phrase from the header (4.5s)
+	setInterval(function () {
+		$("#word").fadeOut(function() {
+			$(this).html(words[i = (i + 1) % words.length]).fadeIn();
+		});
+	}, 3500);
+
+	// handdling the ancre links
+	$(".scroll").click(function() {
+		var section = $("." + this.id);
+		$("html,body").animate({scrollTop: section.offset().top}, "slow");
+	})
+
+	// display of the progress bars (skill)
+	window .sr = ScrollReveal();
+	sr.reveal('.progress-bar', {
+		origin: 'left',
+		duration: 2000,
+		distance: '100%'
+	});
+});
